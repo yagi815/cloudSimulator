938adcd7ce4f43b92fe91449a0e77b93fc634244
yagi815 <yagi815@gmail.com>
2012-09-18 17:10:15 +0900

    init

---
+++ vSinulator/src/test.java
@@ -0,0 +1,17 @@
+
+public class test {
+
+	/**
+	 * Desc :
+	 * @Method Name : main
+	 * @param args
+	 * 
+	 */
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
