1e017ef15718cab1c05a648d7f241b9a190268d1
yagi815 <yagi815@gmail.com>
2012-09-23 20:46:25 +0900

    클라이언트 프로젝트에서 테스트 완료

---
+++ vSimulator_testClient/src/API/API_vcluster.java
@@ -0,0 +1,575 @@
+package API;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.AbstractDocument.BranchElement;
+
+/**
+ * <pre>
+ * API
+ *   |_ API_for_vcluster.java
+ * 
+ * </pre>
+ * 
+ * Desc : Vcluster 가 사용 할 vsimulator 접속하는 API이다. 가상 클라우드인 시뮬레이터에서 필요한 모든 데이터를
+ * 요청하고 받아온다.
+ * 
+ * @Company : KISTI
+ * @Author :grkim
+ * @Date :2012. 8. 10. 오후 2:43:57
+ * @Version: 1.0
+ * 
+ */
+
+public class API_vcluster {
+
+	private static final int PORT = 7878;
+	private static final String IP_ADDR = "127.0.0.1";
+	// private static final String IP_ADDR = "150.183.234.168";
+	private static InetAddress inetAddress = null;
+	private static Socket socket = null;
+
+	private static PrintWriter printWriter = null;
+	private static ObjectInputStream ois = null;
+
+	private static String tmp = "";
+
+	public API_vcluster() {
+		
+//		try {
+//			
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}		
+		
+
+	}
+
+	private static synchronized Object requestToSimulator(String command) {
+		Object result = null;
+		try {
+			
+			socket = new Socket(IP_ADDR, PORT);
+			printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			// 명령 서버로 날림					
+			printWriter.println(command);
+			printWriter.flush();
+
+
+			// 결과 받기
+			ois = new ObjectInputStream(socket.getInputStream());
+			result = ois.readObject();
+			
+			
+			ois.close();
+			printWriter.close();
+			socket.close();
+		} catch (Exception e) {
+			// TODO: handle EXception
+			e.printStackTrace();
+			System.out.println(e.toString());
+		}
+		return result;
+		// return true;
+	}
+
+	// ******************************************************************
+	// 0X: HOSTMACHINE
+	// ******************************************************************
+
+	/**
+	 * Desc : 동작하고 있는 호스트 리스트를 가져온다
+	 * 
+	 * @Method Name : getRunningHostList
+	 * @return 동작하는 호스트 리스트 반환 br <h2>EX) "host01, host02... " <br>
+	 *         String으로 캐스팅해서 사용 (String)list.get(i)
+	 */
+	public List getRunningHostList() {
+		List runningHostList = new ArrayList();
+		runningHostList = (List) requestToSimulator("00:-");
+		return runningHostList;
+	}
+
+	/**
+	 * Desc : 호스트 전원을 켠다
+	 * 
+	 * @Method Name : turnOnHostMachine
+	 * @param hostMachine
+	 *            호스트머신 이름 EX) "host04"
+	 * @return 성공하면 "1", 실패하면 "-1"
+	 * 
+	 */
+	public String turnOnHostMachine(String hostMachine) {
+		return (String) requestToSimulator("01:" + hostMachine);
+	}
+
+	/**
+	 * Desc : 호스트 전원을 끈다.
+	 * 
+	 * @Method Name : turnOffHostMachine
+	 * @param hostMachine
+	 *            호스트머신 이름 EX) "host04"
+	 * @return 성공하면 "1", 실패하면 "-1"
+	 * 
+	 */
+	public String turnOffHostMachine(String hostMachine) {
+		return (String) requestToSimulator("02:" + hostMachine);
+	}
+
+	/**
+	 * Desc : 사용가능한 호스트 리스트
+	 * 
+	 * @Method Name : getAvailableHostList
+	 * @return 사용가능한 호스트 리스트 반환 <br>
+	 *         "host07, host08... " <br>
+	 *         스트링으로 캐스팅해서 사용 (String)list.get(i)
+	 * 
+	 */
+	public List getAvailableHostList() {
+		List availableHostList = new ArrayList();
+		availableHostList = (List) requestToSimulator("03:-");
+		return availableHostList;
+	}
+
+	/**
+	 * Desc : 현재 접속한 클라우드 이름을 가져온다.
+	 * 
+	 * @Method Name : getCloudName
+	 * @return 현재 클라우드 이름 반환 <br>
+	 *         EX) "CloudSinulator"
+	 * 
+	 */
+	public String getCloudName() {
+		return (String) requestToSimulator("04:-");
+	}
+
+	/**
+	 * Desc : 모든 호스트의 리스트
+	 * 
+	 * @Method Name : getHostList
+	 * @return 모든 호스트의 리스트 <br>
+	 *         스트링으로 캐스팅해서 사용 (String)list.get(i)
+	 */
+	public List getHostList() {
+		List hostList = new ArrayList();
+		hostList = (List)requestToSimulator("05:-");
+		return hostList;
+	}
+
+	// ******************************************************************
+	// 2X: VIRTUAL MACHINE
+	// ******************************************************************
+
+	/**
+	 * Desc : 새로운 가상머신을 생성한다.
+	 * 
+	 * @Method Name : createNewVirtualMachine
+	 * @param virtualMachine
+	 *            생성할 가상머신이름 EX) "host03-vm03" <br>
+	 *            parameter가 "-" 이면, 앞에서 순차적으로 생성됨 *
+	 * @return 성공하면 "1", 실패하면 "-1"
+	 * 
+	 */
+	public String createNewVirtualMachine(String virtualMachine) {
+		return (String) requestToSimulator("20:" + virtualMachine);
+	}
+
+	/**
+	 * Desc : 삭제할 가상머신을 생성한다.
+	 * 
+	 * @Method Name : removeVirtualMachine
+	 * @param virtualMachine
+	 *            삭제할 가상머신이름 EX) "host03-vm03"
+	 * @return 성공하면 "1", 실패하면 "-1"
+	 * 
+	 */
+	public String removeVirtualMachine(String virtualMachine) {
+		return (String) requestToSimulator("21:" + virtualMachine);
+	}
+
+	/**
+	 * Desc : 동작하는 가상머신을 새로운 가상머신으로 이동
+	 * 
+	 * @Method Name : migrationVirtualMachine
+	 * @param srcVirtualMachine
+	 *            src 가상머신이름 EX) "host01-vm01"
+	 * @param desVirtualMachine
+	 *            desc 가상머신이름 EX) "host02-vm04"
+	 * @return 성공하면 "1", 실패하면 "-1"
+	 * 
+	 */
+	public String migrationVirtualMachine(String srcVirtualMachine,
+			String desVirtualMachine) {
+		return (String) requestToSimulator("22:" + srcVirtualMachine + ","
+				+ desVirtualMachine);
+	}
+
+	/**
+	 * Desc : 클라우드 내에서 동작중인 가상머신의 리스트를 얻어온다. 호스트-vm정보 의 포멧으로 형식 *
+	 * 
+	 * @Method Name : getRunningVmList
+	 * @return 동작중인 가상머신 리스트를 반환 <br>
+	 *         EX)"host01-vm01,host02-vm05,host07-vm06" <br>
+	 *         스트링으로 캐스팅해서 사용 (String)list.get(i)
+	 * 
+	 */
+	public List getRunningVmList(String hostName) {
+		List runningVmList = new ArrayList();
+		runningVmList = (List) requestToSimulator("23:" + hostName);
+		return runningVmList;
+	}
+
+	/**
+	 * Desc : Available 상태의 가상머신의 리스트를 얻어온다.
+	 * 
+	 * @Method Name : getAvailableVmList
+	 * @return Available 상태의 가상머신 리스트를 반환 <br>
+	 *         EX) "host07-vm10,host07-vm11,host07-vm12" * <br>
+	 *         스트링으로 캐스팅해서 사용 (String)list.get(i)
+	 */
+	public List getAvailableVmList(String hostName) {
+		List availableVmList = new ArrayList();
+		availableVmList = (List) requestToSimulator("24:"+hostName);
+		return availableVmList;
+	}
+
+	/**
+	 * Desc : FAIL 상태의 가상머신의 리스트를 얻어 온다. *
+	 * 
+	 * @Method Name : getFailVmList
+	 * @return FAIL 상태의 가상머신 리스트를 반환 <br>
+	 *         EX) "host07-vm06" * <br>
+	 *         스트링으로 캐스팅해서 사용 (String)list.get(i)
+	 */
+	public List getFailVmList(String hostName) {
+		List failVmList = new ArrayList();
+		failVmList = (List) requestToSimulator("25:"+hostName);
+		return failVmList;
+	}
+
+	/**
+	 * Desc : BUSY 상태의 가상머신의 리스트를 얻어온다. *
+	 * 
+	 * @Method Name : getBusyVmList
+	 * @return BUSY 상태의 가상머신 리스트를 반환 <br>
+	 *         EX) "host07-vm06" <br>
+	 *         스트링으로 캐스팅해서 사용 (String)list.get(i)
+	 */
+	public List getBusyVmList(String hostName) {
+		List failVmList = new ArrayList();
+		failVmList = (List) requestToSimulator("26:"+hostName);
+		return failVmList;
+
+	}
+
+	/**
+	 * Desc : IDLE 상태의 가상머신의 리스트를 얻어온다.
+	 * 
+	 * @Method Name : getIdleVmList
+	 * @return IDLE 상태의 가상머신 리스트를 반환 <br>
+	 *         EX) "host07-vm05" <br>
+	 *         스트링으로 캐스팅해서 사용 (String)list.get(i)
+	 */
+	public List getIdleVmList(String hostName) {
+		List idleVmList = new ArrayList();
+		idleVmList = (List) requestToSimulator("27:"+hostName);
+		return idleVmList;
+	}
+
+	/**
+	 * Desc : unhealthy 상태의 가상머신의 리스트를 얻어온다.
+	 * 
+	 * @Method Name : getUnhealthyVmList
+	 * @return unhealthy 상태의 가상머신 리스트를 반환 <br>
+	 *         EX) "host07-vm01" <br>
+	 *         스트링으로 캐스팅해서 사용 (String)list.get(i)
+	 */
+	public List getUnhealthyVmList(String hostName) {
+		List unHealthyVmList = new ArrayList();
+		unHealthyVmList = (List) requestToSimulator("28:"+hostName);
+		return unHealthyVmList;
+	}
+
+	/**
+	 * Desc : 클라우드 내의 가용한 모든 호스트의 리스트를 얻어온다. *
+	 * 
+	 * @Method Name : getTotalVmList
+	 * @return 가용한 모든 호스트 리스트 들을 반환 <br>
+	 *         EX) "host01-vm01,host02-vm02..." <br>
+	 *         스트링으로 캐스팅해서 사용 (String)list.get(i)
+	 */
+	public List getTotalVmList() {
+		List totalVmList = new ArrayList();
+		totalVmList = (List) requestToSimulator("29:-");
+		return totalVmList;
+	}
+
+	/**
+	 * Desc : Job을 실행중인(busy) 머신 수를 반환
+	 * 
+	 * @Method Name : getRunningJobs
+	 * @return Job을 처리중인 머신수 EX) 80
+	 * 
+	 */
+	public String getRunningJobs(String hostName) {
+		return (String) requestToSimulator("30:"+hostName);
+	}
+
+	/**
+	 * Desc : 최대로 생성가능한 가상머신의 개수를 반환
+	 * 
+	 * @Method Name : getTotalVMs
+	 * @return 최대 생성가능한 가상머신 수 EX) 196
+	 * 
+	 */
+	public String getTotalVMs() {
+		return (String) requestToSimulator("31:-");
+	}
+
+	/**
+	 * Desc : 가상머신의 상태를 반환
+	 * 
+	 * @Method Name : getVmStatus
+	 * @param virtualMachine
+	 *            가성머신 이름 EX) "host01-vm02"
+	 * @return 다음중 하나 반환
+	 *         "pending","prolog","running","shudown","eliplog","stop","null"
+	 */
+	public String getVmStatus(String virtualMachine) {
+		return (String) requestToSimulator("32:" + virtualMachine);
+	}
+
+	/**
+	 * Desc : Job이 수행중인지 확인
+	 * 
+	 * @Method Name : getVMBusy
+	 * @param virtualMachine
+	 *            가상머신 이름 EX) "host01-vm03"
+	 * @return 상태반환 "busy" or "idle"
+	 * 
+	 */
+	public String getVMBusy(String virtualMachine) {
+		return (String) requestToSimulator("33:" + virtualMachine);
+	}
+
+	// ******************************************************************
+	// 4X: VIRTUAL SPEC
+	// ******************************************************************
+
+	/**
+	 * Desc : 가상머신이 런닝한 시간을 가져온다.
+	 * 
+	 * @Method Name : getVMActiveTime
+	 * @param virtualMachine
+	 * @return [시간:분:초 ] 스트링 형식으로 반환 <br>
+	 *         EX) "00:10:13"
+	 * 
+	 */
+	// public String getVMActiveTime(String virtualMachine){
+	// String activeTime = "00:10:13";
+	// return activeTime;
+	// }
+	/**
+	 * 
+	 * Desc : 특정 가상머신의 cpu 스펙을 가져온다.
+	 * 
+	 * @Method Name : getVMCpuInfo
+	 * @param virtualMachine
+	 *            cpu 정보를 가져올 가상 머신 이름을 String 형식으로 입력
+	 * @return cpu 스펙을 스트링으로 반환 <br>
+	 *         EX) "Intel(R) Xeon(R) CPU           E5640  @ 2.67GHz"
+	 * 
+	 */
+	public String getVMCpuInfo(String virtualMachine) {
+		return (String) requestToSimulator("40:" + virtualMachine);
+	}
+
+	/**
+	 * Desc : 특정 가상머신의 mem 정보를 가져온다.
+	 * 
+	 * @Method Name : getVMMemInfo
+	 * @param virtualMachine
+	 *            메모리 정보를 가져올 가상 머신 이름을 String 형식으로 입력
+	 * @return 메모리 정보를 스트링으로 반환 <br>
+	 *         EX) "8Gbyte"
+	 * 
+	 */
+	public String getVMMemInfo(String virtualMachine) {
+		return (String) requestToSimulator("41:" + virtualMachine);
+	}
+
+	/**
+	 * Desc : 특정 가상머신의 디스크 용량을 가져온다.
+	 * 
+	 * @Method Name : getVMDiskInfo
+	 * @param virtualMachine
+	 *            디스크 정보를 가져올 가상 머신 이름을 String 형식으로 입력
+	 * @return 디스크 용량을 스트링으로 반환 <br>
+	 *         EX) "100Gbyte"
+	 * 
+	 */
+	public String getVMDiskInfo(String virtualMachine) {
+		return (String) requestToSimulator("42:" + virtualMachine);
+	}
+
+	/**
+	 * Desc : 특정 가상머신의 OS 정보를 가져온다.
+	 * 
+	 * @Method Name : getVMOSInfo
+	 * @param virtualMachine
+	 *            OS 정보를 가져올 가상 머신 이름을 String 형식으로 입력
+	 * @return OS정보를 스트링으로 반환 <br>
+	 *         EX) "Scientific Linux SLF release 5.7 (Lederman)"
+	 * 
+	 */
+	public String getVMOSInfo(String virtualMachine) {
+		return (String) requestToSimulator("43:" + virtualMachine);
+	}
+
+	/**
+	 * Desc : 특정 가상머신의 OSBIT 정보를 가져온다.
+	 * 
+	 * @Method Name : getVMOSBitInfo
+	 * @param virtualMachine
+	 *            OS BIT 정보를 가져올 가상 머신 이름을 String 형식으로 입력
+	 * @return OSBIT정보를 스트링으로 반환 <br>
+	 *         EX) "Scientific Linux SLF release 5.7 (Lederman)"
+	 * 
+	 */
+	public String getVMOSBitInfo(String virtualMachine) {
+		return (String) requestToSimulator("44:" + virtualMachine);
+	}
+
+	/**
+	 * Desc : 특정 가상머신의 Kernel 정보를 가져온다.
+	 * 
+	 * @Method Name : getVMKernelInfo
+	 * @param virtualMachine
+	 *            커널 정보를 가져올 가상 머신 이름을 String 형식으로 입력
+	 * @return Kernel정보를 스트링 형식으로 반환 <br>
+	 *         EX) "2.6.18-308.11.1.el5"
+	 * 
+	 */
+	public String getVMKernelInfo(String virtualMachine) {
+		return (String) requestToSimulator("45:" + virtualMachine);
+	}
+
+	/**
+	 * Desc : 생성된 가상머신의 고유번호(ID)를 가져온다.
+	 * 
+	 * @Method Name : getVMUUID
+	 * @param virtualMachine
+	 *            UUID를 가져올 머신을 이름을 String 형식으로 입력
+	 * @return VM 고유 번호 스트링 형식으로 반환
+	 * 
+	 */
+	public String getVMUUID(String virtualMachine) {
+		return (String) requestToSimulator("46:" + virtualMachine);
+	}
+
+	// ******************************************************************
+	// MANIPULATION 8X:
+	// ******************************************************************
+	/**
+	 * Desc : Job 수행
+	 * 
+	 * @Method Name : jobSubmit
+	 * @param jobName
+	 *            수행 Job 이름
+	 * @return 성공하면 "1", 실패 "-1"
+	 * 
+	 */
+	public String jobSubmit(String jobName) {
+		return (String) requestToSimulator("80:" + jobName);
+	}
+
+	// ******************************************************************
+	// FOR TEST
+	// ******************************************************************
+
+	/**
+	 * Desc : Test 용 클라우드 출력 함수 * EX> prompt> dump <br>
+	 * ====================================================================<br>
+	 * HostName: host01<br>
+	 * TotalVMs: 12 runningVMs: 3<br>
+	 * --------------------------------------------------------------<br>
+	 * vm ID: 2fa54c42-05c4-4fa4-a70a-87d6f1c69421 vm Name: master vm Status:
+	 * running<br>
+	 * vm ID: 10de73c4-46ae-4cde-af8a-3d3efbbd20a5 vm Name: vm001 vm Status:
+	 * running<br>
+	 * vm ID: 0b056dcf-f286-4d9c-ac63-9a3c2c553e63 vm Name: vm002 vm Status:
+	 * running<br>
+	 * <br>
+	 * ===================================================================<br>
+	 * 
+	 * @Method Name : dumpCloudStatus
+	 * @return 클라우드 상태를 출력 한다.
+	 * 
+	 */
+	public void showHost(String hostName) {
+		System.out.println("hostStatus....");
+		String dumpCloudStatsus = "";
+
+		List runningVmList = getRunningVmList(hostName);
+		List busyVmList = getBusyVmList(hostName);
+		List idleVmList = getIdleVmList(hostName);
+		
+		
+		
+
+		dumpCloudStatsus += "\n===================================================================\n";
+		dumpCloudStatsus += "\n----POWER : ";
+		dumpCloudStatsus += "\n----Running VMs: " + runningVmList.size();
+		dumpCloudStatsus += "\n" + runningVmList;
+		dumpCloudStatsus += "\n----Busy VMs: " + busyVmList.size();
+		dumpCloudStatsus += "\n" + busyVmList;
+		dumpCloudStatsus += "\n----idle VMs: " + idleVmList.size();
+		dumpCloudStatsus += "\n" + idleVmList;
+		dumpCloudStatsus += "\n===================================================================\n";
+		System.out.println(dumpCloudStatsus);
+	}
+
+	public void showVM(String virtualMachine) {
+		String dumpCloudStatus = "";
+		dumpCloudStatus 
+				+= ""
+				+ " Status=" + getVmStatus(virtualMachine) + "\n" 
+				+ " Busy="+ getVMBusy(virtualMachine) + "\n "
+				
+				+ "\n-------------\n"				
+				+ " ID=" + getVMUUID(virtualMachine) + "\n"
+				+ " Cpu="+ getVMCpuInfo(virtualMachine) + "\n"				
+				+ " OS="+ getVMOSInfo(virtualMachine) + "\n" 
+				+ " Disk="+ getVMDiskInfo(virtualMachine) + "\n"; 
+		System.out.println(dumpCloudStatus);
+	}
+
+	public void showCloud() {
+		System.out.println("dumpCloudStatus....");
+		String dumpCloudStatsus = "";
+
+		dumpCloudStatsus += "====================================================================\n"
+				+ "total VMs :"
+				+ getTotalVMs()
+				+ "\n running Vms :"
+				+ getRunningVmList("-").size()
+				+ "\ttotal Running Job :"
+				+ getRunningJobs("-")
+				+ "\ttotal idle VMs :"
+				+ getIdleVmList("-").size() + "\n";
+		dumpCloudStatsus += "\n===================================================================\n";
+
+		System.out.println(dumpCloudStatsus);
+	}
+
+}
---
+++ vSimulator_testClient/src/TestClient.java
@@ -0,0 +1,202 @@
+
+
+import java.util.Scanner;
+
+import API.API_vcluster;
+
+/**
+ * <pre>
+ * SimulatorMain
+ *   |_ client.java
+ * </pre>
+ *
+ * Desc :
+ * @Company : KISTI
+ * @Author :grkim
+ * @Date   :2012. 7. 31. 오후 1:50:00
+ * @Version:
+ *
+ */
+public class TestClient {
+
+	
+	private static boolean IS_EXIT = true;
+	private static Scanner scanner = null;
+	private static String command = null;
+	
+	static API_vcluster API = null;
+	
+	public TestClient() {
+		// TODO Auto-generated constructor stub		
+
+	}	
+	
+	
+	public static void executeCommand(String command, String parameter){
+		
+		/*
+		 * cloudinfo
+		 * 클라우드 정보를 출력 
+		 * Total host:       
+		 * Total vms:	runningVms:		idleVms:	    
+		 * 
+		 * list vms 
+		 * 시뮬레이터에 올라가잇는 모든 가상머신 리스트
+		 * 
+		 * list runningvms
+		 * 전원이 들어가서 동작중인 가상머신 리스트 
+		 * 
+		 * list busyvms
+		 * job을 수행중인 가상머신 리스트
+		 * 
+		 *  
+		 * list hosts
+		 * 시뮬레이터에 있는 물리적인 호스트 들의 리스트
+		 * 
+		 * list failvms
+		 * fail vm 리스트 
+		 * 
+		 * list availvms
+		 * available vm 리스트 
+		 * 
+		 * list idlevms 
+		 * idle vm 리스트
+		 *  
+		 * 
+		 * 
+		 * showvm 'vm-name'
+		 * showhost 
+		 * dump 형식으로 출력
+		 * showhost 'hostname'
+		 * dump 형식으로 출력하되 한 호스트만 
+		 * 
+		 * starthost 'hostname'
+		 * 호스트 전원 켜기
+		 * 
+		 * stophost 'hostname'
+		 * 호스트 전원끄기 
+		 * 
+		 * startvm 'vm-name'
+		 * vm 추가 생성
+		 * 
+		 * stopvm 'vm-name'
+		 * vm 전원 끄기 / 삭제 
+		 *		 * 
+		 * job-submit 'job-name'
+		 * job 수행  
+		 * 
+		 * 
+		 * 
+		 * */
+		
+		
+		
+		if (command.contains("list")) {
+			
+			if ( parameter.equals("vms") ) {
+				System.out.println(API.getRunningVmList("-"));
+			}			
+			if(parameter.equals("runningvms")){
+				System.out.println(API.getRunningVmList("-"));					
+			}
+			if (parameter.equals("busyvms")) {
+				System.out.println(API.getBusyVmList("-"));				
+			}
+			if (parameter.equals("hosts")) {				
+				System.out.println(API.getHostList());				
+			}
+			if (parameter.equals("runninghosts")) {				
+				System.out.println(API.getRunningHostList());				
+			}
+			if (parameter.equals("availhosts")) {				
+				System.out.println(API.getAvailableHostList());				
+			}			
+			if (parameter.equals("failvms")) {				
+				System.out.println(API.getFailVmList("-"));				
+			}
+			if (parameter.equals("idlevms")) {				
+				System.out.println(API.getIdleVmList("-"));				
+			}
+			if (parameter.equals("availvms")) {				
+				System.out.println(API.getAvailableVmList("-"));				
+			}			
+		}		
+		
+		if (command.contains("createvm")) {		
+			System.out.println(API.createNewVirtualMachine(parameter));			
+		}
+		if (command.contains("delvm")) {
+			System.out.println(API.removeVirtualMachine(parameter));
+		}
+		
+			
+		if (command.equals("showvm")) {
+			if (parameter.equals("-")) {
+				System.out.println("input vmname");
+			}else {
+				API.showVM(parameter);
+			}				
+		}
+		if (command.equals("showhost")) {
+			
+			if (parameter.equals("-")) {
+				System.out.println("input hostname");
+			}else {
+				API.showHost(parameter);
+			}				
+		}
+		if (command.equals("showcloud")) {
+			API.showCloud();
+		}
+	
+	
+		if (command.contains("starthost")) {
+			System.out.println(API.turnOnHostMachine(parameter));
+		}
+		if (command.contains("stophost")) {
+			System.out.println(API.turnOffHostMachine(parameter));			
+		}
+//		if (command.contains("starvm")) {
+//			
+//		}
+//		if (command.contains("stopvm")) {
+//			
+//		}
+		
+		
+		
+	}
+	
+	public static void main(String[] args){
+		
+		API = new API_vcluster();
+		
+		String result="";
+		scanner =  new Scanner(System.in);
+		
+
+		while (IS_EXIT) {
+	
+			System.out.print("prompt> ");
+			command = scanner.nextLine();
+			
+			if (command.equals("quit") || command.equals("exit")) {				
+				IS_EXIT = false;
+			} else {
+//				if (command.contains(" ")) {
+					
+					String[] tmp = command.split("[ ]");
+					if (tmp.length > 1) {
+						
+						executeCommand(tmp[0], tmp[1]);
+					}
+					if (tmp.length == 1) {
+						executeCommand(tmp[0], "-");
+					}
+//				}else {
+//					executeCommand(command, "");
+//				}
+			}
+		}		
+	}
+}
